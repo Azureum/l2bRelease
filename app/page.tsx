a1cf8baeca9f452d9e1d9ced2a8dce6bbd4d5689
Azurieum <mlgopnope@gmail.com>
2024-06-15 14:51:07 -0400

    for jason

--- app/page.tsx
+++ app/page.tsx
@@ -8,7 +8,7 @@ import { BackgroundGradient } from "../app/components/ui/background-gradient";
 import { BentoGrid, BentoGridItem } from "../app/components/ui/bento-grid";
 export default function App() {
 return (
-<main>
+<main className='App dark'>
    <section>
       <div className='w-screen h-screen grid md:grid-cols-2'>
          <div className="grid h-screen place-items-center ">
@@ -29,19 +29,19 @@ return (
                      <p className="text-base sm:text-4xl text-black mt-4 mb-2 dark:text-neutral-200 font-bold">
                         Participants
                      </p>
-                     <p className="text-3xl text-black ">
+                     <p className="text-3xl dark:text-indigo-400	-200 font-bold">
                         3,381
                      </p>
                      <p className="text-base sm:text-4xl text-black mt-4 mb-2 dark:text-neutral-200 font-bold">
                         Schedule
                      </p>
-                     <p className="text-xl text-black">
+                     <p className="text-xl dark:text-neutral-200">
                         The Hackathon will go from,  <span className='font-bold'>July 15 to September 20.</span> You can still become a participant  when the hackathon is active.
                      </p>
                      <p className="text-base sm:text-4xl text-black mt-4 mb-2 dark:text-neutral-200 font-bold">
                         Prize Pool
                      </p>
-                     <p className="text-xl text-black">
+                     <p className="text-xl dark:text-neutral-200">
                         $25,000 USD +
                         Other Awards
                      </p>
@@ -53,14 +53,51 @@ return (
    </section>
    <section>
       <div>
-      
+         {/* Create one for contact, About, some statistics */}
+      <BentoGrid className="max-w-4xl mx-auto md:auto-rows-[20rem]">
+      {items.map((item, i) => (
+        <BentoGridItem
+          key={i}
+          title={item.title}
+          description={item.description}
+          header={item.header}
+          className={item.className}
+        />
+      ))}
+    </BentoGrid>
       </div>
    </section>
    <BackgroundBeams />
 </main>
 );
 }
-
 const Skeleton = () => (
-  <div className="flex flex-1 w-full h-full min-h-[6rem] rounded-xl   dark:bg-dot-white/[0.2] bg-dot-black/[0.2] [mask-image:radial-gradient(ellipse_at_center,white,transparent)]  border border-transparent dark:border-white/[0.2] bg-neutral-100 dark:bg-black"></div>
-);
+   <div className="flex flex-1 w-full h-full min-h-[6rem] rounded-xl   dark:bg-dot-white/[0.2] bg-dot-black/[0.2] [mask-image:radial-gradient(ellipse_at_center,white,transparent)]  border border-transparent dark:border-white/[0.2] bg-neutral-100 dark:bg-black"></div>
+ );
+ const items = [
+   {
+     title: "The Dawn of Innovation",
+     description: "Explore the birth of groundbreaking ideas and inventions.",
+     header: <Skeleton />,
+     className: "md:col-span-2",
+   },
+   {
+     title: "The Digital Revolution",
+     description: "Dive into the transformative power of technology.",
+     header: <Skeleton />,
+     className: "md:col-span-1",
+   },
+   {
+     title: "The Art of Design",
+     description: "Discover the beauty of thoughtful and functional design.",
+     header: <Skeleton />,
+     className: "md:col-span-1",
+   },
+   {
+     title: "The Power of Communication",
+     description:
+       "Understand the impact of effective communication in our lives.",
+     header: <Skeleton />,
+     className: "md:col-span-2",
+   },
+ ];
