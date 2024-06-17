d60825649e648d5dc0ae17e834ba8ff17ea9ab7f
Azurieum <mlgopnope@gmail.com>
2024-06-17 13:19:50 -0400

    block screen fully done(3d)

--- app/blocked/page.tsx
+++ app/blocked/page.tsx
@@ -6,9 +6,8 @@ export default function Blocked() {
       <div className="text-center">
         <h1 className="text-2xl font-bold">Excess access detected</h1>
         <p className="mt-4">You have exceeded the rate limit. Please try again in a few seconds.</p>
-        <p className="mt-2 ">If you did not mean to for this to happen, don't worry this is only a security measure.😸</p>
+        <p className="mt-2 ">If you did not mean for this to happen, don&apos;t worry this is only a security measure.😸</p>
       </div>
     </div>
   );
-};
-
+}
