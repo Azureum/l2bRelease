5ce46a989e51e7fd683ef405b9ac98693cd7daf5
Azurieum <mlgopnope@gmail.com>
2024-06-19 19:26:00 -0400

    date change

--- app/mobile/page.tsx
+++ app/mobile/page.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 
 export default function Blocked() {
+  document.body.style.overflow = 'hidden';
   return (
     <div className="flex h-screen justify-center items-center">
       <div className="text-center">
         <h1 className="text-2xl font-bold">Mobile User Detected</h1>
-        <p className="mt-4">Sorry! We currently are working on mobile support.</p>
+        <p className="mt-4">Sorry! We are currently working on mobile support.</p>
         <p className="mt-2">Our engineer is working hard to bring it to life, you can still access the website on laptop/desktop</p>
         <p className="mt-2">our engineer: 🔧😿 </p>
       </div>
